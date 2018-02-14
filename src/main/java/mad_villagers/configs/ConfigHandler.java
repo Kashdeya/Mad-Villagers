bde69b8759b7b89cd93c5832b6d5b33d3a2cbb2b
vadis365 <curve_linear@hotmail.com>
2018-02-14 22:24:40 +0000

    more stuff

--- src/main/java/mad_villagers/configs/ConfigHandler.java
+++ src/main/java/mad_villagers/configs/ConfigHandler.java
@@ -14,6 +14,9 @@ public class ConfigHandler {
 	public static float MAD_RANGE;
 	public static int DISTANCE_FROM_VILLAGE;
 	public static String[] BLACKLISTED_BLOCKS;	
+	public static float ATTACK_DAMAGE;
+	public static boolean CALL_FOR_HELP;
+	public static boolean LONG_TERM_MEMORY;
 	public final String[] usedCategories = { "Mad Villagers" };
 
 	public void loadConfig(FMLPreInitializationEvent event) {
@@ -28,6 +31,10 @@ public class ConfigHandler {
         MAD_RANGE = CONFIG.getFloat("Mad Villager Range", "Mad Villagers", 30, 1, 100, "Distance in blocks Villagers notice blocks being broken");
         MAD_CHANCE = CONFIG.getInt("Mad Villager Range From Village", "Mad Villagers", 32, 8, 128, "Distance in Blocks around The Village Villagers aggro");
         BLACKLISTED_BLOCKS = CONFIG.getStringList("Blocks broken ignored by Villagers", "Mad Villagers", new String[] { "minecraft:obsidian"}, "For Sanity");
+
+        LONG_TERM_MEMORY = CONFIG.getBoolean("Mad Villager Long Term Memory", "Mad Villagers", true, "Enable Long Term Memory?");
+        CALL_FOR_HELP = CONFIG.getBoolean("Mad Villagers Call for Help", "Mad Villagers", true, "Enable Call for Help?");
+        ATTACK_DAMAGE = CONFIG.getFloat("Mad Villager Attack Damage", "Mad Villagers", 2F, 1F, 10F, "Sets Attack Damage");
 
         if (CONFIG.hasChanged())
 			CONFIG.save();
--- src/main/java/mad_villagers/utils/Events.java
+++ src/main/java/mad_villagers/utils/Events.java
@@ -5,11 +5,20 @@ import java.util.List;
 
 import mad_villagers.configs.ConfigHandler;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.village.Village;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -35,7 +44,7 @@ public class Events {
 			}
 		}
 	}
-	
+
 	public boolean isNearVillage(EntityVillager entity) {
 		Village village = entity.getEntityWorld().getVillageCollection().getNearestVillage(new BlockPos(entity), ConfigHandler.DISTANCE_FROM_VILLAGE);
 		if (village == null)
@@ -54,5 +63,42 @@ public class Events {
 		if(blockList.contains(block))
 			return true;
 		return false;
+	}
+
+	@SubscribeEvent
+	public void onEntityJoinedWorld(EntityJoinWorldEvent event) {
+		if (event.getEntity() instanceof EntityVillager) {
+			EntityVillager entity = (EntityVillager) event.getEntity();
+			World world = entity.getEntityWorld();
+			if (!world.isRemote)
+				setUpAI(entity);
+		}
+	}
+
+	public void setUpAI(EntityCreature entity) {
+		entity.tasks.addTask(1, new AIVillagerAttack(entity, 1.25D, ConfigHandler.LONG_TERM_MEMORY));
+		entity.targetTasks.addTask(1, new EntityAIHurtByTarget(entity, ConfigHandler.CALL_FOR_HELP));
+	}
+
+	static class AIVillagerAttack extends EntityAIAttackMelee {
+		public AIVillagerAttack(EntityCreature entity, double moveSpeed, boolean longMemory) {
+			super(entity, moveSpeed, longMemory);
+		}
+
+		@Override
+		protected void checkAndPerformAttack(EntityLivingBase entity, double distanceIn) {
+			double reach = this.getAttackReachSqr(entity);
+			if (distanceIn <= reach && this.attackTick <= 0) {
+				attackTick = 20;
+				attacker.swingArm(EnumHand.MAIN_HAND);
+
+				// This overrides the vanilla method with out needing to fuck about with entities
+					attackEntityAsMob(attacker, entity); // Use our damage value attack below
+			}
+		}
+
+		public boolean attackEntityAsMob(EntityCreature entityAttacker, Entity entityIn) {
+			return entityIn.attackEntityFrom(DamageSource.causeMobDamage(entityAttacker), ConfigHandler.ATTACK_DAMAGE);
+		}
 	}
 }
